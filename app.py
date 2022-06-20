3be1a02248ffc9f269171961e8d0aa552c8cdce7
sk.abdul haq <75989562+skabdulhaq@users.noreply.github.com>
2022-06-20 18:08:00 +0530

    Revert "Update app.py"
    This reverts commit c09975f2b56448cf7f3f9e19248e68020ba1d21d.

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, redirect
-import requests
+import random, requests
+import gunicorn
 
 app = Flask(__name__)
 
 
-@app.route("/<index>")
-def manager(index):
-    url = "https://api.npoint.io/83316894f4c0ca891969"
+@app.route("/")
+def home():
+    url = "https://api.npoint.io/ebbdbf2551af841022dc"
     list_website =  requests.get(url).json()["list"]
-    return redirect(list_website[int(index)])
+    return redirect(random.choice(list_website))
 
 if __name__ == "__main__":
     app.run()
